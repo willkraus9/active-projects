f09eb6631ea1217244c79c0902023ad20aed1ede
Will Kraus <will.kraus9@gmail.com>
2023-06-15 15:57:34 -0400

    Setup for Python Course
    Setting up Python course + intro section

---
+++ Python Course/Python Course Artifacts/helloworld.py
@@ -0,0 +1,14 @@
+print("I love pizza!")
+print("It is really good.")
+
+first_name = "Will"
+last_name = "Kraus"
+full_name = first_name + " " + last_name
+#print ("Hello, " + full_name)
+age = 22
+age +=1
+print (type(age))
+
+
+
+
